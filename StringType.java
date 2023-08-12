e892c1c36f8d8fbc1a36d52f47af05637b7b7e4f
gopalakrishnan <semsampathkumar@gmail.com>
2023-08-12 11:29:36 +0530

    Added string data type and casting

---
+++ StringType.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class StringType {
+    public static void main(String[] args) {
+        // String is non-primitive type and we can use methods
+        String name = "good Morning";
+        System.out.println(name.toUpperCase());
+    }
+}
---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        // TypeCasting
+        int a = 2;
+        double b = a;
+        System.out.println(b);
+
+        long e = 2323;
+        byte g = (byte) e;
+        System.out.println(g);
+    }
+}
